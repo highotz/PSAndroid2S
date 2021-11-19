623eaa257c60407d6ce01433aac18984c3b3ec08
Higor Hotz Vasconcelos <highotz.hotz@hotmail.com>
2021-11-19 18:23:08 -0300

    fix to login with email

--- app/src/main/java/com/example/psandroid2s/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/example/psandroid2s/ui/signin/SignInFragment.kt
@@ -1,13 +1,20 @@
 package com.example.psandroid2s.ui.signin
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.psandroid2s.R
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignInFragment : Fragment() {
 
@@ -19,8 +26,6 @@ class SignInFragment : Fragment() {
     private var REQUEST_CODE = 1001
     private val providers = arrayListOf(
         AuthUI.IdpConfig.EmailBuilder().build(),
-        AuthUI.IdpConfig.GoogleBuilder().build(),
-        AuthUI.IdpConfig.GitHubBuilder().build(),
     )
 
     override fun onCreateView(
@@ -38,6 +43,22 @@ class SignInFragment : Fragment() {
         )
 
         return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQUEST_CODE){
+            val response = IdpResponse.fromResultIntent(data)
+
+            if(resultCode == Activity.RESULT_OK){
+                val firebaseUser = Firebase.auth.currentUser
+                Log.i("FirebaseUIResult", "${firebaseUser?.email}")
+                Toast.makeText(requireActivity().applicationContext, "Falha no Login!", Toast.LENGTH_LONG).show()
+            }else  {
+                Toast.makeText(requireActivity().applicationContext, "Falha no Login!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
