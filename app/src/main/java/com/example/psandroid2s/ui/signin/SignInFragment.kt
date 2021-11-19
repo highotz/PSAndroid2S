4465941f6125b734f991acf8cabec238da8f22af
Higor Hotz Vasconcelos <highotz.hotz@hotmail.com>
2021-11-19 17:21:02 -0300

    Initial commit

---
+++ app/src/main/java/com/example/psandroid2s/ui/signin/SignInFragment.kt
@@ -0,0 +1,48 @@
+package com.example.psandroid2s.ui.signin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.psandroid2s.R
+import com.firebase.ui.auth.AuthUI
+
+class SignInFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SignInFragment()
+    }
+
+    private lateinit var viewModel: SignInViewModel
+    private var REQUEST_CODE = 1001
+    private val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        AuthUI.IdpConfig.GoogleBuilder().build(),
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view = inflater.inflate(R.layout.sign_in_fragment, container, false)
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            REQUEST_CODE
+        )
+
+        return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SignInViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
