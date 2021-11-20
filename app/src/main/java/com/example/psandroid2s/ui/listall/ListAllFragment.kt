9abfbf7b6ad3f266261d9460f6b0db7c228aa5b2
Higor Hotz Vasconcelos <highotz.hotz@hotmail.com>
2021-11-20 00:14:33 -0300

    checkpoint

--- app/src/main/java/com/example/psandroid2s/ui/listall/ListAllFragment.kt
+++ app/src/main/java/com/example/psandroid2s/ui/listall/ListAllFragment.kt
@@ -8,15 +8,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.GeneratedAdapter
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.psandroid2s.R
 import com.example.psandroid2s.factory.ViewModelFactory
 import com.example.psandroid2s.model.Local
 import com.example.psandroid2s.model.ReturnRelatorio
+import com.example.psandroid2s.ui.medicoesadpt.MedicoesListAdapter
 import kotlin.math.log
 
 class ListAllFragment : Fragment() {
 
-    //private lateinit var adapter: GuarulhosListAdapter
+    private lateinit var adapter: MedicoesListAdapter
 
     private val viewModel: ListAllViewModel by lazy {
         ViewModelProvider(this,
@@ -28,15 +31,20 @@ class ListAllFragment : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.list_all_fragment, container, false)
+        val recyclerview = view.findViewById<RecyclerView>(R.id.rv_medicoesList)
+
+        adapter = MedicoesListAdapter()
+        recyclerview.adapter = adapter
+        recyclerview.layoutManager = LinearLayoutManager(this.activity)
 
         viewModel.getAll()
         viewModel.listMedicoes.observe(viewLifecycleOwner){
-            it.forEach { Log.e("ListaAll", it.id )}
-
+            adapter.items = it
         }
         return view
     }
--- app/src/main/java/com/example/psandroid2s/ui/medicoesadpt/MedicoesListAdapter.kt
+++ app/src/main/java/com/example/psandroid2s/ui/medicoesadpt/MedicoesListAdapter.kt
@@ -1,4 +1,88 @@
 package com.example.psandroid2s.ui.medicoesadpt
 
-class MedicoesListAdapter() {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.psandroid2s.R
+import com.example.psandroid2s.model.ReturnRelatorio
+import com.example.psandroid2s.ui.listall.ListAllViewModel
+
+class MedicoesListAdapter :
+    RecyclerView.Adapter<MedicoesListAdapter.RelatorioListItemViewHolder>() {
+
+    var items = emptyList<ReturnRelatorio>()
+        set(value) {
+            val result = DiffUtil.calculateDiff(
+                RelatorioListDiffCallback(
+                    field,
+                    value
+                )
+            )
+            result.dispatchUpdatesTo(this)
+            field = value
+        }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RelatorioListItemViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.medicoes_item, parent, false)
+
+        return RelatorioListItemViewHolder(
+            view
+        )
+    }
+
+    override fun onBindViewHolder(holder: RelatorioListItemViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    fun getItemAt(position: Int): ReturnRelatorio {
+        return items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class RelatorioListItemViewHolder(
+        itemView: View
+    ) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(items: ReturnRelatorio) {
+            itemView.findViewById<TextView>(R.id.txt_cidade).text = items.sensor.local.cidade
+            itemView.findViewById<TextView>(R.id.txt_co2).text = items.co2Vol.toString()
+            itemView.findViewById<TextView>(R.id.txt_data).text = items.medicaoDate
+            itemView.findViewById<TextView>(R.id.txt_local).text = items.sensor.local.nome
+            itemView.findViewById<TextView>(R.id.txt_ph).text = items.phVol.toString()
+        }
+
+    }
+
+    class RelatorioListDiffCallback(
+        private val oldList: List<ReturnRelatorio>,
+        private val newList: List<ReturnRelatorio>
+    ) : DiffUtil.Callback() {
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].id == newList[newItemPosition].id
+        }
+
+        override fun getOldListSize(): Int {
+            return oldList.size
+        }
+
+        override fun getNewListSize(): Int {
+            return newList.size
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return true
+        }
+    }
 }
+
+
+
